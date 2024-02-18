7d6f0269796f2b5d4fc2b341661a17d881786f1f
ClaytonNolen <nolenc@southwestern.edu>
2024-02-18 15:00:25 -0600

    Add and Delete to DB Debugging with HTML page.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@
 
 import { initializeApp } from 'firebase/app'
 import {
-  getFirestore,
-  collection,
-  getDocs
+  getFirestore, collection, getDocs,
+  addDoc,
+  deleteDoc,
+  doc
 } from 'firebase/firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -27,7 +28,7 @@ initializeApp(firebaseConfig)
 const db = getFirestore()
 
 // Collection Ref.
-const collectionRef = collection(db, 'x')
+const collectionRef = collection(db, 'test')
 
 // Get Collection Data.
 getDocs(collectionRef)
@@ -40,4 +41,33 @@ getDocs(collectionRef)
   })
   .catch(err => {
     console.log(err.message)
+})
+
+// Adding Documents.
+const addTestForm = document.querySelector('.add')
+addTestForm?.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(collectionRef, {
+    name: addTestForm.name.value,
+    age: addTestForm.age.value,
+    membership: addTestForm.membership.value
+  })
+  .then(() => {
+    addTestForm.reset()
+  })
+})
+
+// Deleting Documents.
+const deleteTestForm = document.querySelector('.delete')
+deleteTestForm?.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+const documentRef = doc(db, 'test', deleteTestForm.id.value)
+
+deleteDoc(documentRef)
+  .then(() => {
+    deleteTestForm.reset()
+  })
+
 })
